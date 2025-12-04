25fda2f298004fff4b34250a8a5355df8da9231b
tithoft <99927615+tithoft@users.noreply.github.com>
2025-12-04 14:16:18 -0500

    Added additional page for viewing all blogs. Base
    template also created.

---
+++ blogs/urls.py
@@ -0,0 +1,12 @@
+"""Defines URL patterns for blogs."""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'blogs'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
+    path('blogs/', views.blogs, name='blogs'),
+]
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import Blog
+
+def index(request):
+    """The home page for the Blog."""
+    return render(request, 'blogs/index.html')
+
+def blogs(request):
+    """Page to view all blogs."""
+    blogs = Blog.objects.all()
+    context = {'blogs': blogs}
+    return render(request, 'blogs/blogs.html', context)
